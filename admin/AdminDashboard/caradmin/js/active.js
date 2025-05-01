a9b11fa55c5f5ce3a02e736f5008107402e607b5
hmdee <32852463+hmdee@users.noreply.github.com>
2025-05-01 23:16:45 +0300

    new1 admin

--- admin/AdminDashboard/caradmin/js/active.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const currentUrl = window.location.href;
-    document.querySelectorAll('#menu .nav-link').forEach(link => {
-      link.classList.toggle('active', link.href === currentUrl);
-    });
-  });
