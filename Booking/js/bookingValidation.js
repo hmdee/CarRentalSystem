d05b4669170f57fd009c29b6f02ffb799aab41c6
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2025-05-01 04:31:43 +0300

    Booking

---
+++ Booking/js/bookingValidation.js
@@ -0,0 +1,38 @@
+const pickUpDateError = document.getElementById("pickUpDateError");
+const DropOffDateError = document.getElementById("DropOffDateError");
+export default function validateAllInputs(
+  pickUpDate,
+  DropOffDate,
+  pickUpTime,
+  dropOffTime
+) {
+  if (pickUpDate && DropOffDate && pickUpTime && dropOffTime) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export function DateValidation(pickUpDate, pickUpTime,DropOffDate,dropOffTime) {
+    let isValid=true
+  const now = new Date();
+  const pickUpDateTime = new Date(`${pickUpDate} ${pickUpTime}`);
+  const dropOffDateTime = new Date(`${DropOffDate} ${dropOffTime}`);
+  if (pickUpDateTime < now) {
+        pickUpDateError.innerText=`You can't choose an old date`
+        isValid=false
+        return
+  } else {
+    pickUpDateError.innerText=``
+  }
+  if (dropOffDateTime<pickUpDateTime) {
+    DropOffDateError.innerText=`Drop off Date must be after pick up date`
+    isValid=false
+    return
+  }
+  else
+  {
+    DropOffDateError.innerText=''
+  }
+  return isValid
+}
